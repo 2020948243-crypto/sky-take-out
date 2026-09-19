937f238e4a31d526be993d6ae5e47f8493bebc22
liuliaofeng <liuliaofeng@qq.com>
2026-09-19 14:09:35 +0800

    查看购物车代码开发

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -65,4 +65,11 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
 
         }
     }
+
+    @Override
+    public List<ShoppingCart> showShoppingCart() {
+        return shoppingCartMapper.list(ShoppingCart.builder()
+        .userId(BaseContext.getCurrentId())
+                .build());
+    }
 }
