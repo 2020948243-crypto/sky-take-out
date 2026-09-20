f5228e103cad0b5821fa2c5d38f195d8c0e8d150
liuliaofeng <liuliaofeng@qq.com>
2026-09-20 13:05:42 +0800

    清空购物车代码开发

--- sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -45,4 +45,16 @@ public class ShoppingCartController {
     public Result<List<ShoppingCart>> list() {
         return Result.success(shoppingCartService.showShoppingCart());
     }
+
+    /*
+    *
+    * 清空购物车商品
+    *
+    * */
+    @DeleteMapping("/clean")
+    @ApiOperation("清空购物车商品")
+    public Result clean(){
+        shoppingCartService.cleanShoppingCart();
+        return Result.success();
+    }
 }
--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -20,4 +20,11 @@ public interface ShoppingCartService {
     *
     * */
     List<ShoppingCart> showShoppingCart();
+
+    /*
+    *
+    * 清空购物车商品
+    *
+    * */
+    void cleanShoppingCart();
 }
