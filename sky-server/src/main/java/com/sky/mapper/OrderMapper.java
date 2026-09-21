f8e98286b72c35d15da7cda8d9c7fb50d99a0ed9
liuliaofeng <liuliaofeng@qq.com>
2026-09-21 17:14:49 +0800

    订单状态定时处理

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -5,6 +5,9 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Mapper
 public interface OrderMapper {
 
@@ -29,4 +32,12 @@ public interface OrderMapper {
     *
     * */
     void update(Orders orders);
+
+    /*
+    *
+    *根据状态和下单时间查询订单
+    *
+    * */
+    @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
 }
